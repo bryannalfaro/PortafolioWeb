cd7155331fb9ed86f086d04149c6383d3ccc44e9
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-05-02 17:46:44 -0600

    making first things

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,48 +1,24 @@
 import React from 'react'
-import Particles from 'react-particles-js'
+import Typed from 'react-typed'
+import ParticleC from '../Particle/ParticleCreate'
 import '../../styles/App.css'
 
 const App = () => (
-  <div className="padre" style={{ position: 'absolute', width: '100%', height: '100%' }}>
-    <div className="App" style={{ position: 'absolute', width: '100%', height: '100%' }}>
-      <Particles
-        params={{
-          particles: {
-            number: {
-              value: 50,
-              density: {
-                enable: true,
-                value_area: 1000,
-              },
-            },
-
-          },
-          interactivity: {
-            detect_on: 'canvas',
-            events: {
-              onhover: {
-                enable: true,
-                mode: 'repulse',
-              },
-              onclick: {
-                enable: true,
-                mode: 'push',
-              },
-              resize: true,
-            },
-          },
-          line_linked: {
-            enable: false,
-          },
-          shape: {
-            type: 'circle',
-          },
-        }}
-      />
-    </div>
-    <h1 className="title">WEB DEVELOPER</h1>
+  <div className="padre" style={{ position: 'fixed', width: '100%', height: '100%' }}>
+    <ParticleC />
     <div className="container">
       <div className="perfil" />
+      <Typed
+        strings={[
+          'I am a <strong>web developer</strong>',
+          'I am a <strong>visionary</strong>',
+          'I am <strong>your best choice</strong>',
+        ]}
+        typeSpeed={60}
+        backSpeed={50}
+        loop
+        className="typed"
+      />
     </div>
   </div>
 
---
+++ src/components/Particle/ParticleCreate.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Particles from 'react-particles-js'
+
+const ParticleC = () => (
+  <div className="app">
+    <Particles
+      params={{
+        particles: {
+          number: {
+            value: 50,
+            density: {
+              enable: true,
+              value_area: 1000,
+            },
+          },
+
+        },
+        interactivity: {
+          detect_on: 'window',
+          events: {
+            onhover: {
+              enable: true,
+              mode: 'repulse',
+            },
+            onclick: {
+              enable: true,
+              mode: 'push',
+            },
+            resize: true,
+          },
+        },
+        line_linked: {
+          enable: false,
+        },
+        shape: {
+          type: 'circle',
+        },
+      }}
+    />
+  </div>
+)
+
+export default ParticleC
