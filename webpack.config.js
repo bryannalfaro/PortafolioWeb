b3110cad43f5527efe928b876d280b39985f320c
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-05-02 12:36:08 -0600

    configuration for portfolio

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(jsx?)$/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    templateContent: `
+    <html>
+      <head>
+
+        <meta charset="UTF-8">
+
+      </head>
+
+      <body>
+        <div id="root" />
+      </body>
+    </html>
+  `,
+  }), new MiniCssExtractPlugin()],
+}
